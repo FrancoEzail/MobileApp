86ef6510c1e2544cf6fe8a71c260ce3048bac9ec
FrancoEzail <francoezail@gmail.com>
2023-12-01 12:39:27 +0300

    add stats

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+      <IonHeader class='toolbar'>
+        <IonToolbar color="dark">
+        <IonImg src="/ws_logo.svg" alt="Logo" class='logo'/>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+          <IonImg src="/ws_logo.svg" alt="Logo" />
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
